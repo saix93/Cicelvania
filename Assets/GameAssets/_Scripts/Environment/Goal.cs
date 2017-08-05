b932fa595ad09570a1a3b8e373ac296ace79785e
Saix_93 <r.torresgonzalez.93@gmail.com>
2017-08-05 18:44:30 +0200

    .

---
+++ Assets/GameAssets/_Scripts/Environment/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Simon simon = collision.GetComponent<Simon>();
+
+        if (simon)
+        {
+            SceneManager.LoadScene("Title", LoadSceneMode.Single);
+        }
+    }
+}
